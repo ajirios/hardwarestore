febbfff018ba3e55db0ab9f7273d416e42f6cbbf
AJIRI OSAUZO JEFFREY <ajirios@peppermultimedia.com>
2019-09-17 06:47:26 -0500

    Add files via upload

---
+++ A4Osauzo7682469.java
@@ -0,0 +1,100 @@
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class A4Osauzo7682469
+  
+{
+  public static void main(String[] args)
+    
+  {
+    BufferedReader file;
+    String fileName;
+    
+    
+  }
+  
+}
+
+
+class Table
+  
+{
+  class Node
+    
+  {
+    ProductRecord record;
+    String key;
+    
+    public Node(ProductRecord record, String key)
+      
+    {
+      
+      this.record = record;
+      this.key = key;
+      
+    }
+    
+  }
+  
+  Node[] table;
+  
+  public Table(int size)
+    
+  {
+    
+    table = new Node[size];
+    
+  }
+  
+  public void insert(String key, ProductRecord record)
+    
+  {
+    
+    
+    
+  }
+  
+  public ProductRecord search(String key)
+    
+  {
+    
+    
+    
+  }
+  
+  public void printAllMatches(String key)
+    
+  {
+    
+    
+    
+  }
+  
+  public void printTable()
+    
+  {
+    
+    
+    
+  }
+  
+}
+
+class ProductRecord
+  
+{
+  
+  public ProductRecord()
+    
+  {
+    
+    
+    
+  }
+  
+}
+
+
